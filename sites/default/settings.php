2f3b011353bffa8f588799615d55784c553f9c90
Marcelo Tosco <marcelo@front.id>
2017-03-11 17:26:53 +0100

    Añado settings faltantes.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -717,3 +717,7 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
+
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_zBiBuwdS1vNimAVZQ0KacOFJGKRNnrGrjYnY8ACqfEUnudMp3tRPukSEnjGm7v15TmWat-igrw/sync';
+$config_directories['conf'] = 'sites/default/conf';
