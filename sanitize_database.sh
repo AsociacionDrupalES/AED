6bfa2eeb8ae4ce34bbe0e3a73624033f74f5d13f
Alvaro Hurtado <alvaro.hurtado.villegas@gmail.com>
2017-06-01 22:26:49 +0200

    Clean the code.

--- sanitize_database.sh
+++ sanitize_database.sh
@@ -20,14 +20,17 @@ echo -e "${LBLUE} > Sanitizing core fields.${DEFAULT}"
 drush sqlsan --sanitize-password=12345 --sanitize-email=dr%nid@drupal.es
 
 echo
-echo -e "${LBLUE} > Updating non-core fields.${DEFAULT}"
+echo -e "${LBLUE} > Table user__field_social_links.${DEFAULT}"
 drush sqlq "UPDATE user__field_social_links SET field_social_links_title = substring(MD5(RAND()), -8);"
 drush sqlq "UPDATE user__field_social_links SET field_social_links_uri = concat('https://www.drupal.org/', RAND());"
 
+echo -e "${LBLUE} > Table url_alias.${DEFAULT}"
 drush sqlq "UPDATE url_alias SET alias = concat('/user/', RAND()) where source LIKE '%user%'"
 
+echo -e "${LBLUE} > Table user__field_name.${DEFAULT}"
 drush sqlq "UPDATE user__field_name SET field_name_value = concat('Name ', RAND())"
 
+echo -e "${LBLUE} > Table users_field_data.${DEFAULT}"
 drush sqlq "UPDATE users_field_data SET name = concat('u', uid) WHERE uid NOT IN (0,1)"
 drush sqlq "UPDATE users_field_data SET mail = concat(uid, '@drupal.es') WHERE uid NOT IN (0,1)"
 drush sqlq "UPDATE users_field_data SET init = concat(uid, '@drupal.es') WHERE uid NOT IN (0,1)"
