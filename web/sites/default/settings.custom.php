a4dd11d8b7674961a20c678f1b57a000cb377d09
Jonathan Araña Cruz <jonathan@cambrico.net>
2026-05-22 17:39:23 +0200

    Add settings to set paypal and email config from Amazee env variables

--- web/sites/default/settings.custom.php
+++ web/sites/default/settings.custom.php
@@ -1,5 +1,14 @@
 <?php
 
+$config['paypal_sdk.settings']['environment'] = getenv('paypal__environment');
+$config['paypal_sdk.settings']['live_client_id'] = getenv('paypal__live_client_id');
+$config['paypal_sdk.settings']['live_client_secret'] = getenv('paypal__live_client_secret');
+$config['paypal_sdk.settings']['sandbox_client_id'] = getenv('paypal__sandbox_client_id');
+$config['paypal_sdk.settings']['sandbox_client_secret'] = getenv('paypal__sandbox_client_secret');
+
+$config['symfony_mailer_lite.symfony_mailer_lite_transport.smtp']['configuration']['user'] = getenv('smtp__user');
+$config['symfony_mailer_lite.symfony_mailer_lite_transport.smtp']['configuration']['pass'] = getenv('smtp__pass');
+
 // Disable Lagoon logs if not on Amazee.
 if (!getenv('LAGOON')) {
   $config['lagoon_logs.settings']['disable'] = 1;
